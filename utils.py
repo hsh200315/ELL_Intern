b079a24ece5edd01d3d035a02cd413ec0e073919
hsh200315 <hsh200315@naver.com>
2024-09-06 06:28:34 +0000

    Add epoch arg

--- utils.py
+++ utils.py
@@ -7,5 +7,6 @@ def add_args():
 	parser.add_argument("-d", "--dataset", dest="dataset",action="store")
 	parser.add_argument("-b", "--batch_size", dest="batch_size",action="store")
 	parser.add_argument("-l", "--lr", dest="lr",action="store")
+	parser.add_argument("-e", "--epoch", dest="epoch", action="store")
 
 	return parser.parse_args()
