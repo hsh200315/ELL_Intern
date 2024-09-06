60225108cc4e853b48a821faf2f83e4d00ebf249
hsh200315 <hsh200315@naver.com>
2024-09-06 05:23:31 +0000

    Add argparse

---
+++ utils.py
@@ -0,0 +1,11 @@
+import argparse
+
+def add_args():
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-m", "--model", dest="model", action="store")
+	parser.add_argument("-d", "--dataset", dest="dataset",action="store")
+	parser.add_argument("-b", "--batch_size", dest="batch_size",action="store")
+	parser.add_argument("-l", "--lr", dest="lr",action="store")
+
+	return parser.parse_args()
