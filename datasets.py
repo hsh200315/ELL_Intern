86798cc67c52136815771fd43b49b014d68d8629
hsh200315 <hsh200315@naver.com>
2024-09-08 04:53:32 +0000

    Data size 변수화

--- datasets.py
+++ datasets.py
@@ -3,7 +3,7 @@ from torch.utils.data import Dataset
 from torchvision import datasets
 import torchvision.transforms as transforms
 
-def load_data(dataset):
+def load_data(dataset, size):
 	dataset = dataset.lower()
 	if dataset == "cifar10":
 		train_data = datasets.CIFAR10(
@@ -11,7 +11,7 @@ def load_data(dataset):
 			train=True,
 			download=True,
 			transform= transforms.Compose([
-				transforms.RandomResizedCrop(32),
+				transforms.RandomResizedCrop(size),
 				transforms.RandomHorizontalFlip(),
 				transforms.ToTensor(),
 				transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
@@ -22,7 +22,7 @@ def load_data(dataset):
 			train=False,
 			download=True,
 			transform= transforms.Compose([
-				transforms.Resize(32),
+				transforms.Resize(size),
 				transforms.ToTensor(),
 				transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 			])
@@ -50,6 +50,7 @@ def load_data(dataset):
 			download=True,
 			transform= transforms.Compose([
 				transforms.Resize(32),
+				transforms.ToTensor(),
 				transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 			])
 		)
