f85e123f5a3617fdd8ad20483c8aefedbd7c80be
hsh200315 <hsh200315@naver.com>
2024-09-21 11:12:32 +0000

    DenseNet 오류 수정 및 간결화

--- models.py
+++ models.py
@@ -243,15 +243,18 @@ class PreActResNet(nn.Module):
 class DenseTransitionLayer(nn.Module):
     def __init__(self, input_channel, theta=0.5):
         super(DenseTransitionLayer, self).__init__()
+        
+        self.bn = nn.BatchNorm2d(input_channel)
+        self.relu = nn.ReLU(inplace=True)
         self.conv = nn.Conv2d(input_channel, int(theta*input_channel), kernel_size=1, stride=1, padding=0)
         self.avgpool = nn.AvgPool2d((2,2), 2)
     
     def forward(self, x):
-        x = self.conv(x)
+        x = self.conv(self.relu(self.bn(x)))
         x = self.avgpool(x)
         
         return x
-    
+
 class DenseBottleNeck(nn.Module):
     def __init__(self, input_channel, growth_rate):
         super(DenseBottleNeck, self).__init__()
@@ -267,7 +270,7 @@ class DenseBottleNeck(nn.Module):
         x = self.conv2(self.relu(self.bn2(x)))
         
         return x
-    
+
 class DenseBlock(nn.Module):
     def __init__(self, input_channel, growth_rate, num_layer):
         super(DenseBlock, self).__init__()
@@ -278,7 +281,6 @@ class DenseBlock(nn.Module):
             self.bottlenecks.append(DenseBottleNeck(input_channel+i*growth_rate, growth_rate))
             
     def forward(self, x):
-        count = 1
         feature_map = [x]
         for i in range(self.num_layer):
             x = self.bottlenecks[i](torch.cat(feature_map, 1))
@@ -292,37 +294,36 @@ class DenseNet(nn.Module):
         input_channel = [start_channel]
         
         self.conv1 = nn.Conv2d(3, input_channel[0], kernel_size=3, stride=1, padding=1)
-        self.dblock1 = DenseBlock(input_channel[0], growth_rate, layer_num[0])
-        self.tlayer1 = DenseTransitionLayer(input_channel[0]+growth_rate*layer_num[0], theta)
-        input_channel.append(int((input_channel[0]+growth_rate*layer_num[0])*theta))
-        self.dblock2 = DenseBlock(input_channel[1], growth_rate, layer_num[1])
-        self.tlayer2 = DenseTransitionLayer(input_channel[1]+growth_rate*layer_num[1], theta)
-        input_channel.append(int((input_channel[1]+growth_rate*layer_num[1])*theta))
-        self.dblock3 = DenseBlock(input_channel[2], growth_rate, layer_num[2])
-        self.tlayer3 = DenseTransitionLayer(input_channel[2]+growth_rate*layer_num[2], theta); 
-        input_channel.append(int((input_channel[2]+growth_rate*layer_num[2])*theta))
+        
+        self.dense_block = nn.ModuleList()
+        self.transition_layer = nn.ModuleList()
+        for i in range(2):
+            self.dense_block.append(DenseBlock(input_channel[i], growth_rate, layer_num[i]))
+            self.transition_layer.append(DenseTransitionLayer(input_channel[i]+growth_rate*layer_num[i], theta))
+            input_channel.append(int((input_channel[i]+growth_rate*layer_num[i])*theta))
+        self.dense_block.append(DenseBlock(input_channel[2], growth_rate, layer_num[2]))
         
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        num = (input_channel[1]-input_channel[0]) + (input_channel[2]-input_channel[1]) + (input_channel[3]-input_channel[2]) + start_channel
+        num = input_channel[2] + growth_rate*layer_num[2]
+        self.bn = nn.BatchNorm2d(num)
+        self.relu = nn.ReLU(inplace=True)
         self.fc = nn.Linear(num, 10)
     
     def forward(self, x):
         x = self.conv1(x)
-        x = self.dblock1(x)
-        x = self.tlayer1(x)
-        x = self.dblock2(x)
-        x = self.tlayer2(x)
-        x = self.dblock3(x)
-        x = self.tlayer3(x)
-        x = self.avgpool(x)
+        for i in range(2):
+            x = self.dense_block[i](x)
+            x = self.transition_layer[i](x)
+        x = self.dense_block[2](x)
         
+        x = self.avgpool(self.relu(self.bn(x)))
         x = torch.flatten(x,1)
         x = self.fc(x)
         
         return x
 
 
-    
+
 class FractalBasicBlock(nn.Module):
     def __init__(self, input_channel, output_channel):
         super(FractalBasicBlock, self).__init__()
