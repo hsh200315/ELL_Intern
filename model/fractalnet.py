5c68ce48d40036bfdf02a2c4e801ef6457e68522
hsh200315 <hsh200315@naver.com>
2024-09-25 08:37:28 +0000

    Fractalnet dropout layer추가

--- model/fractalnet.py
+++ model/fractalnet.py
@@ -4,11 +4,12 @@ import torch.nn.functional as F
 import random
 
 class ConvLayer(nn.Module):
-    def __init__(self, input_channel, output_channel):
+    def __init__(self, input_channel, output_channel, drop_out_prob):
         super(ConvLayer, self).__init__()
         self.conv = nn.Conv2d(input_channel, output_channel, kernel_size=3, padding=1, stride=1)
         self.bn = nn.BatchNorm2d(output_channel)
         self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(p=drop_out_prob)
         
     def forward(self, x):
         x = self.relu(self.bn(self.conv(x)))
@@ -22,17 +23,16 @@ class JoinLayer(nn.Module):
         return (input1+input2)/2
 
 class FractalBlock(nn.Module):
-    def __init__(self, input_channel, output_channel, depth, local_prob, global_prob):
+    def __init__(self, input_channel, output_channel, depth, local_prob, drop_out_prob):
         super(FractalBlock, self).__init__()
         self.depth = depth
         self.local_prob = local_prob
-        self.global_prob = global_prob
         if depth == 1:
-            self.block = ConvLayer(input_channel, output_channel)
+            self.block = ConvLayer(input_channel, output_channel, drop_out_prob)
         else:
             self.short_path = ConvLayer(input_channel, output_channel)
-            self.block1 = FractalBlock(input_channel, output_channel, depth-1)
-            self.block2 = FractalBlock(output_channel, output_channel, depth-1)
+            self.block1 = FractalBlock(input_channel, output_channel, depth-1, local_prob, drop_out_prob)
+            self.block2 = FractalBlock(output_channel, output_channel, depth-1, local_prob, drop_out_prob)
             self.join = JoinLayer()
             
     def forward(self, x, epoch):
@@ -61,9 +61,9 @@ class FractalNet(nn.Module):
         super(FractalNet, self).__init__()
         self.B = B
         self.blocks = nn.ModuleList()
-        self.blocks.append(FractalBlock(3, start_channel, C))
+        self.blocks.append(FractalBlock(3, start_channel, C, 0.5, 0))
         for i in range(B-1):
-            self.blocks.append(FractalBlock(start_channel*pow(2,i), start_channel*pow(2,i+1), C))
+            self.blocks.append(FractalBlock(start_channel*pow(2,i), start_channel*pow(2,i+1), C, 0.5, 0.1*(i+1)))
         
         self.maxpool = nn.MaxPool2d((2,2))
         self.fc = nn.Linear(start_channel*pow(2,B-1), 10)
