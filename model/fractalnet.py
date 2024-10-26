0537957e027071df26ce80dc81a77227c0f22b6c
hsh200315 <hsh200315@naver.com>
2024-10-26 14:19:08 +0000

    오류 수정

--- model/fractalnet.py
+++ model/fractalnet.py
@@ -6,6 +6,7 @@ import random
 torch.manual_seed(42)
 random.seed(42)
 
+#Conv 검토 완료
 class ConvLayer(nn.Module):
     def __init__(self, input_channel, output_channel, drop_out_prob):
         super(ConvLayer, self).__init__()
@@ -13,8 +14,6 @@ class ConvLayer(nn.Module):
         self.bn = nn.BatchNorm2d(output_channel)
         self.relu = nn.ReLU(inplace=True)
         self.dropout = nn.Dropout(p=drop_out_prob)
-        
-        nn.init.xavier_uniform_(self.conv.weight)
         
     def forward(self, x):
         x = self.dropout(self.relu(self.bn(self.conv(x))))
@@ -29,25 +28,27 @@ class JoinLayer(nn.Module):
     def forward(self, inputs, is_global, path_num):
         selected = []
         local_drop = []
-        length = len(inputs)
         if self.training:
             if is_global: #global drop path
                 if self.depth <= path_num:
                     selected.append(inputs[0])
                 if path_num == 1 or self.depth > path_num:
-                    selected.append(inputs[length-1]) #global drop path
+                    selected.append(inputs[self.depth-path_num]) #global drop path 오류
             else:
-                for i in range(length):#local drop path
+                for i in range(self.depth):#local drop path
                     local_drop.append(1)
                     if random.random() < self.lprob:
                         local_drop[i] = 0
                 if sum(local_drop) == 0:
-                    local_drop[random.randint(0, length-1)] = 1
-                for i in range(length):
+                    local_drop[random.randint(0, self.depth-1)] = 1
+                for i in range(self.depth):
                     if local_drop[i] == 1:
                         selected.append(inputs[i]) # local drop path
         else:
             selected = inputs
+            
+        if len(selected) == 1:
+            return selected[0]
         x = torch.stack(selected, dim=0).mean(dim=0)
         return x
 
@@ -82,7 +83,7 @@ class FractalNet(nn.Module):
         self.C = C
         self.block_list = nn.ModuleList()
         self.block_list.append(FractalBlock(3, start_channel, C, 0.15, 0.0))
-        self.join = JoinLayer(B, 0.15)
+        self.join = JoinLayer(C, 0.15)
         for i in range(B-1):
             if start_channel*(2**i+1) <= end_channel:
                 self.block_list.append(FractalBlock(start_channel*(2**i), start_channel*(2**(i+1)), C, 0.15, 0.1*(i+1)))
